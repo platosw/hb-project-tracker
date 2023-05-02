ba80be3f68bcb1b7b1093b5e8294860d917460dd
Jason Park <platosw83@gmail.com>
2023-05-02 12:58:51 -0400

    Completed with Tyler

--- test.sql
+++ test.sql
@@ -29,3 +29,58 @@
 --          ('Tyler1', 'Game2', 100);
 
 -- DELETE FROM projects WHERE title = 'Tyler1' OR title = 'Jason1' OR title = 'Jason';
+
+-- CREATE VIEW report_card_view AS
+-- SELECT students.first_name,
+--        students.last_name,
+--        projects.title,
+--        projects.max_grade,
+--        grades.grade
+-- FROM students
+--   JOIN grades ON (students.github = grades.student_github)
+--   JOIN projects ON (projects.title = grades.project_title);
+
+
+
+
+-- CREATE TABLE grades (
+--   id SERIAL PRIMARY KEY,
+--   student_github VARCHAR(30) 
+--     REFERENCES students,
+--   project_title VARCHAR(30)
+--     REFERENCES projects,
+--   grade INTEGER
+-- );
+
+-- INSERT INTO grades (student_github, project_title, grade)
+--   VALUES ('jhacks', 'Markov', 10),
+--          ('jhacks', 'Markov', 2),
+--          ('sdevelops', 'Markov', 50),
+--          ('sdevelops', 'Blockly', 100);
+
+-- -- INSERT INTO students VALUES ('jhacks', 'Jane', 'Hacker');
+
+-- INSERT INTO grades VALUES (DEFAULT, 'Jason', 'Markov', 1000);
+
+-- SELECT first_name, last_name
+-- FROM students
+-- WHERE github = 'jhacks';
+
+-- SELECT project_title, grade
+-- FROM grades
+-- WHERE student_github = 'jhacks';
+
+-- SELECT title, max_grade
+-- FROM projects;
+
+-- SELECT 
+--   first_name,
+--   last_name,
+--   project_title,
+--   grade,
+--   max_grade
+-- FROM students
+--   JOIN grades ON (students.github = grades.student_github)
+--   JOIN projects ON (grades.project_title = projects.title)
+-- WHERE github = 'jhacks';
+
