d93bcee9e64c93b344cd19851079c09e3d6c74f8
Jason Park <platosw83@gmail.com>
2023-05-02 07:11:56 -0400

    completed further study

--- project-tracker.sql
+++ project-tracker.sql
@@ -21,6 +21,42 @@ SET default_tablespace = '';
 SET default_table_access_method = heap;
 
 --
+-- Name: grades; Type: TABLE; Schema: public; Owner: platosw83
+--
+
+CREATE TABLE public.grades (
+    id integer NOT NULL,
+    student_github character varying(30),
+    project_title character varying(30),
+    grade integer
+);
+
+
+ALTER TABLE public.grades OWNER TO platosw83;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE; Schema: public; Owner: platosw83
+--
+
+CREATE SEQUENCE public.grades_id_seq
+    AS integer
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER TABLE public.grades_id_seq OWNER TO platosw83;
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: platosw83
+--
+
+ALTER SEQUENCE public.grades_id_seq OWNED BY public.grades.id;
+
+
+--
 -- Name: projects; Type: TABLE; Schema: public; Owner: platosw83
 --
 
@@ -34,17 +70,52 @@ CREATE TABLE public.projects (
 ALTER TABLE public.projects OWNER TO platosw83;
 
 --
+-- Name: report_card_view; Type: TABLE; Schema: public; Owner: platosw83
+--
+
+CREATE TABLE public.report_card_view (
+    first_name character varying(30),
+    last_name character varying(30),
+    title character varying(30),
+    max_grade integer,
+    grade integer
+);
+
+
+ALTER TABLE public.report_card_view OWNER TO platosw83;
+
+--
 -- Name: students; Type: TABLE; Schema: public; Owner: platosw83
 --
 
 CREATE TABLE public.students (
     github character varying(30) NOT NULL,
     first_name character varying(30),
-    last_name character varying(30)
+    last_name character varying(30),
+    cohort character varying(20)
 );
 
 
 ALTER TABLE public.students OWNER TO platosw83;
+
+--
+-- Name: grades id; Type: DEFAULT; Schema: public; Owner: platosw83
+--
+
+ALTER TABLE ONLY public.grades ALTER COLUMN id SET DEFAULT nextval('public.grades_id_seq'::regclass);
+
+
+--
+-- Data for Name: grades; Type: TABLE DATA; Schema: public; Owner: platosw83
+--
+
+COPY public.grades (id, student_github, project_title, grade) FROM stdin;
+1	jhacks	Markov	10
+2	jhacks	Blockly	2
+3	sdevelops	Markov	50
+4	sdevelops	Blockly	100
+\.
+
 
 --
 -- Data for Name: projects; Type: TABLE DATA; Schema: public; Owner: platosw83
@@ -57,13 +128,40 @@ Blockly	Programmatic Logic Puzzle Game	100
 
 
 --
+-- Data for Name: report_card_view; Type: TABLE DATA; Schema: public; Owner: platosw83
+--
+
+COPY public.report_card_view (first_name, last_name, title, max_grade, grade) FROM stdin;
+Jane	Hacker	Markov	50	10
+Jane	Hacker	Blockly	100	2
+Sarah	Developer	Markov	50	50
+Sarah	Developer	Blockly	100	100
+\.
+
+
+--
 -- Data for Name: students; Type: TABLE DATA; Schema: public; Owner: platosw83
 --
 
-COPY public.students (github, first_name, last_name) FROM stdin;
-jhacks	Jane	Hacker
-sdevelops	Sarah	Developer
+COPY public.students (github, first_name, last_name, cohort) FROM stdin;
+jhacks	Jane	Hacker	\N
+sdevelops	Sarah	Developer	\N
 \.
+
+
+--
+-- Name: grades_id_seq; Type: SEQUENCE SET; Schema: public; Owner: platosw83
+--
+
+SELECT pg_catalog.setval('public.grades_id_seq', 4, true);
+
+
+--
+-- Name: grades grades_pkey; Type: CONSTRAINT; Schema: public; Owner: platosw83
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_pkey PRIMARY KEY (id);
 
 
 --
@@ -80,6 +178,22 @@ ALTER TABLE ONLY public.projects
 
 ALTER TABLE ONLY public.students
     ADD CONSTRAINT students_pkey PRIMARY KEY (github);
+
+
+--
+-- Name: grades grades_project_title_fkey; Type: FK CONSTRAINT; Schema: public; Owner: platosw83
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_project_title_fkey FOREIGN KEY (project_title) REFERENCES public.projects(title);
+
+
+--
+-- Name: grades grades_student_github_fkey; Type: FK CONSTRAINT; Schema: public; Owner: platosw83
+--
+
+ALTER TABLE ONLY public.grades
+    ADD CONSTRAINT grades_student_github_fkey FOREIGN KEY (student_github) REFERENCES public.students(github);
 
 
 --
